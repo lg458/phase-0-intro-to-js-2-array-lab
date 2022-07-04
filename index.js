0330ac1153d9af243f3c1d7d40d68959a93992a2
Laura Gonzalez <lg458@cornell.edu>
2022-07-03 18:05:56 -0700

    commit tree 958debe6fb3c179d2315025d94df7460bc065787

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
 const cats = ["Milo", "Otis", "Garfield"]; 
 
-//destructively appends a cat to the end of the cats array
 function destructivelyAppendCat(name) { 
-   cats.push(name);
+  cats.push(name);
 } 
 
-//destructively prepends a cat to the beginning of the cats array
 function destructivelyPrependCat(name) { 
   cats.unshift(name);
 } 
 
-//destructively removes the last cat from the cats array 
 function destructivelyRemoveLastCat() { 
   cats.pop();
 } 
 
-//destructively removes the first cat from the cats array
 function destructivelyRemoveFirstCat() { 
   cats.shift();
 } 
 
-//non destructively appends a cat to the cats array and returns a new array 
-function appendCat(name) { 
-  const newCats = [...cats, name]; 
-  return newCats;
+function appendCat(name) {
+  return [...cats, name];
 } 
 
-//nondestructively prepends a cat to the cats array and returns a new array
 function prependCat(name) { 
-  const newCats = [name, ...cats]; 
-  return newCats;
+  return [name, ...cats];
 } 
 
-//nondestructively removes the last cat in the cats array and returns a new array
 function removeLastCat() { 
   return cats.slice(0, -1);
 } 
 
-//nondestructively removes the first cat from the cats array and returns a new array 
 function removeFirstCat() { 
   return cats.slice(1);
-}
+} 
